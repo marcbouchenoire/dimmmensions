55e8e9217741ce371937a56b256adb8c758a901c
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-08-17 16:06:58 +0200

    Add root redirect

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   basePath: "/ios-dimensions",
   cleanUrls: true,
-  trailingSlash: false
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ios-dimensions",
+        permanent: true,
+        basePath: false
+      }
+    ]
+  }
 }
