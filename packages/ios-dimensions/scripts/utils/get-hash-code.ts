0ed7db0978b4352e5652655e8f83e8b4f896a86b
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-09-13 18:25:57 +0200

    Lint files

--- packages/ios-dimensions/scripts/utils/get-hash-code.ts
+++ packages/ios-dimensions/scripts/utils/get-hash-code.ts
@@ -3,7 +3,7 @@ import { Dimensions } from "../../src/types"
 export function getHashCode(dimensions: Dimensions) {
   let hashCode = 0
 
-  for (const character of [...JSON.stringify(dimensions)]) {
+  for (const character of JSON.stringify(dimensions)) {
     hashCode = Math.trunc(Math.imul(31, hashCode) + character.charCodeAt(0))
   }
 
