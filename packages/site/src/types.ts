1eac67d4bdd8c8b3c39aea09f107fb8c9f3b398e
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-28 23:33:41 +0100

    Fix linting with missing data

--- packages/site/src/types.ts
+++ packages/site/src/types.ts
@@ -1,3 +1,15 @@
+export interface Data {
+  /**
+   * The current year.
+   */
+  date: string
+
+  /**
+   * The latest package version.
+   */
+  version: string
+}
+
 export type Intersection<U> = (U extends any ? (k: U) => void : never) extends (
   k: infer I
 ) => void
